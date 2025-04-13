1f181fae381ef569bc6d63180e9158d6f1fac356
Larissa <l.carvaborges@gmail.com>
2025-04-12 21:12:22 -0300

    git commit -m "Sua mensagem de commit"

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+       dados (args);
+    
 }
+    
+        public static void dados (String [] args){
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Por favor, insira o númera da conta: ");
+            int numero = scanner.nextInt();
+
+            scanner.nextLine();
+
+            System.out.println("Por favor, insira o o número da sua agência: ");
+            String agencia = scanner.nextLine();
+            
+            scanner.nextLine();
+
+            System.out.println("Por favor, digite o nome do cliente: ");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o saldo da conta: ");
+            double saldo = scanner.nextDouble();
+
+            System.out.println("\n Olá "+ nomeCliente + "! Obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", sua conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque!");
+            
+        }
+    }
+
